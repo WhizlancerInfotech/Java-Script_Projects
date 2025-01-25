ec33bc89c0f5c6bec675b353b413af009fd4c2a1
WhizlancerInfotech <developer@whizlancer.com>
2025-01-25 02:37:29 -0800

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,19 @@
+// Backend (server.js)
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => console.log('Server running on port 3000'));
