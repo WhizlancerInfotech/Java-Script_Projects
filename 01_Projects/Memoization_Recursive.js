959228d74a8088d59e2c778afbef9c0d8318ede6
WhizlancerInfotech <developer@whizlancer.com>
2025-02-14 05:10:22 -0800

    Create Memoization_Recursive.js

---
+++ 01_Projects/Memoization_Recursive.js
@@ -0,0 +1,13 @@
+function memoize(fn) {
+    const cache = new Map();
+    return function (...args) {
+        const key = JSON.stringify(args);
+        if (cache.has(key)) return cache.get(key);
+        const result = fn(...args);
+        cache.set(key, result);
+        return result;
+    };
+}
+
+const fibonacci = memoize(n => (n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2)));
+console.log(fibonacci(50));
